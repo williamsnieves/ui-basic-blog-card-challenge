e3461b783a0e76cf3d1db44fb11c79179f7da40f
Willians Nieves <williansnieves@gmail.com>
2025-05-17 11:23:49 +0200

    fix: correct file paths in index.html and vite.config.ts for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/blog-card-challenge/",
   plugins: [react(), tailwindcss()],
-  base: '/blog-card-challenge/', // Set this to your repo name for GitHub Pages
 });
